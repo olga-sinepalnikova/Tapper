ee8e779d6fcc1af9764031cb259a4882228c431e
Olga Sinepalnikova <olgasinepalnikova2@gmail.com>
2024-09-04 21:03:44 +0300

    First try with templates

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -12,18 +13,52 @@ type User struct {
 	TokensCount uint   `json:"tokens_count"`
 }
 
+type Message struct {
+	SenderId   uint   `json:"sender_id"`
+	ReceiverId uint   `json:"receiver_id"`
+	Text       string `json:"text"`
+}
+
 var users = []User{
 	{0, "Some Body", "C00l_p@ss", 200},
 	{1, "No Body", "not_so_cool_pass", 20},
 	{2, "Just Mind", "I-H@t3_Pa5Se5", 1_000_000},
 }
 
+var messages = []Message{
+	{0, 1, "Ur password suck"},
+	{1, 0, "U haven't seen JustMinds password"},
+	{2, 0, "U haven't seen my password"},
+}
+
 func getUser(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
 }
 
+func getMessages(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, messages)
+}
+
+func postMessage(c *gin.Context) {
+	var msg Message
+	if err := c.ShouldBindJSON(&msg); err != nil {
+		return
+	}
+	messages = append(messages, msg)
+	fmt.Println(messages)
+	c.IndentedJSON(http.StatusCreated, msg)
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", getUser)
+	router.LoadHTMLGlob("web/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Tapper first page",
+		})
+	})
+	router.GET("/users", getUser)
+	router.GET("/msg", getMessages)
+	router.POST("/msg", postMessage)
 	router.Run("localhost:8081")
 }
